e91ed14ef6771ea03294fac72797429497d35400
Gaurav Tyagi <grvtyagi22@gmail.com>
2025-05-03 22:31:33 +0530

    Use guard pattern to guard the mutex

--- examples/hello_redis.rs
+++ examples/hello_redis.rs
@@ -4,7 +4,7 @@ use mini_redis::{Result, client::connect};
 async fn main() -> Result<()> {
     let mut client = connect("127.0.0.1:6363").await?;
 
-    // client.set("foo", "value".into()).await?;
+    client.set("foo", "value".into()).await?;
 
     let res = client.get("foo").await?;
 
--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,24 @@ use tokio::net::TcpListener;
 
 type Db = Arc<Mutex<HashMap<String, Bytes>>>;
 
+struct DbGuard {
+    db: Db,
+}
+
+impl DbGuard {
+    fn new(db: Db) -> Self {
+        Self { db }
+    }
+
+    fn insert(&self, key: String, value: Bytes) {
+        self.db.lock().unwrap().insert(key, value);
+    }
+
+    fn get<'a>(&'a self, key: &'a str) -> Option<Bytes> {
+        self.db.lock().unwrap().get(key).map(|v| v.to_owned())
+    }
+}
+
 #[tokio::main]
 async fn main() {
     let port = "127.0.0.1:6363";
@@ -24,21 +42,20 @@ async fn main() {
 }
 
 async fn process(socket: tokio::net::TcpStream, db: Db) {
-    // let mut db = HashMap::new();
     let mut conn = Connection::new(socket);
+
+    let db = DbGuard::new(db);
 
     while let Some(frame) = conn.read_frame().await.unwrap() {
         let frame = match Command::from_frame(frame).unwrap() {
             Command::Set(cmd) => {
-                {
-                    let mut db = db.lock().unwrap();
-                    let value = Bytes::copy_from_slice(cmd.value());
-                    db.insert(cmd.key().to_string(), value);
-                }
+                let value = Bytes::copy_from_slice(cmd.value());
+                db.insert(cmd.key().to_string(), value);
+
                 Frame::Simple("OK".to_string())
             }
             Command::Get(cmd) => {
-                if let Some(value) = db.lock().unwrap().get(cmd.key()) {
+                if let Some(value) = db.get(cmd.key()) {
                     Frame::Bulk(value.clone().into())
                 } else {
                     Frame::Null
